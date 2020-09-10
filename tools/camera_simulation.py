422b40dd8b0b7fc231fe6009c292f1f5dbeda365
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2020-09-10 16:40:02 +0300

    developing camera simulation

--- tools/camera_simulation.py
+++ tools/camera_simulation.py
@@ -14,20 +14,18 @@ from keras.callbacks import ModelCheckpoint
 import glob
 from PIL import Image
 import numpy as np
-import cv2
 from skimage import img_as_uint
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
-#config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30)
-config.enable_stream(rs.stream.infrared, 1, 848, 480, rs.format.y8, 30) # 1 = left
+config.enable_stream(rs.stream.infrared, 1, 848, 480, rs.format.y8, 30) # 1 for left frame
 # Start streaming
 pipeline.start(config)
 
 
-def test(noisy_image, ir_image):
+def predict(noisy_image, ir_image):
     channels = 2
     cropped_w, cropped_h = 480, 480
     test_model_name = r"C:\work\OLD ML\expirements_models_denoised\NEW DATA - NO MASK\4443 images\unet model - 100 epochs - strides 1500 - NO mask - Binary - WITH IR - NOT FINISHED\DEPTH_20200903-132536.model_new"
@@ -53,10 +51,8 @@ def test(noisy_image, ir_image):
     for i in range(len(cropped_ir)):
         noisy_images_plt = cropped_noisy[i].reshape(1, cropped_w, cropped_h, 1)
         ir_images_plt = cropped_ir[i].reshape(1, cropped_w, cropped_h, 1)
-
         im_and_ir = np.stack((noisy_images_plt, ir_images_plt), axis=3)
         im_and_ir = im_and_ir.reshape(1, cropped_w, cropped_h, channels)
-
         img = np.array(im_and_ir)
         # Parse numbers as floats
         img = img.astype('float32')
@@ -65,17 +61,8 @@ def test(noisy_image, ir_image):
         #img = (img - np.average(img)) / np.var(img)
         img = img / 65535
         sample = img
-
         row, col = cropped_image_offsets[i]
-        # Show sample
-        #cv2.imwrite(r"C:\work\ML_git\tmp\sample"+str(i)+".png", img_as_uint(sample[0,:,:,0]))
-        #cv2.imwrite(r"C:\work\ML_git\tmp\sample" + str(i+10) + ".png", img_as_uint(sample[0, :, :, 1]))
-
         denoised_image = model.predict(sample)
-        # Show predicted
-        #cv2.imwrite(r"C:\work\ML_git\tmp\predicted"+str(i)+".png", img_as_uint(denoised_image[0, :, :, 0]))
-        #cv2.imwrite(r"C:\work\ML_git\tmp\predicted" + str(i+10) + ".png", img_as_uint(denoised_image[0, :, :, 1]))
-
         row_end = row + cropped_h
         col_end = col + cropped_w
         denoised_row = cropped_h
@@ -88,19 +75,31 @@ def test(noisy_image, ir_image):
             denoised_col = abs(col - col_end)
         # combine tested images
         whole_image[row:row_end, col:col_end] = denoised_image[:, 0:denoised_row, 0:denoised_col, :]
-        #cv2.imwrite(r"C:\work\ML_git\tmp\whole_image" + str(i) + ".png", img_as_uint(whole_image[:, :, 0]))
     return  img_as_uint(whole_image[:, :, 0])
-    #cv2.imwrite(r"C:\work\ML_git\tmp\im1.png", whole_image[:, :, 0])
-
 
 #=============================================================================================================
+def convert_image(i):
+    m = np.min(i)
+    M = np.max(i)
+    i = np.divide(i, np.array([M - m], dtype=np.float)).astype(np.float)
+    i = (i - m).astype(np.float)
+    i8 = (i * 255.0).astype(np.uint8)
+    if i8.ndim == 3:
+        i8 = cv2.cvtColor(i8, cv2.COLOR_BGRA2GRAY)
+    i8 = cv2.equalizeHist(i8)
+    colorized = cv2.applyColorMap(i8, cv2.COLORMAP_JET)
+    colorized[i8 == int(m)] = 0
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    colorized = cv2.putText(colorized, str(m) + " .. " + str(M), (20, 50), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    return colorized
+
+
 try:
     while True:
 
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
-        #color_frame = frames.get_color_frame()
         ir_frame = frames.get_infrared_frame()
 
         if not depth_frame or not ir_frame:
@@ -108,20 +107,18 @@ try:
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
-        #color_image = np.asanyarray(color_frame.get_data())
         ir_image = np.asanyarray(ir_frame.get_data())
-
-        predicted_image = test(depth_image, ir_image)
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        predicted_image = predict(depth_image, ir_image)
 
         # Stack both images horizontally
+        depth_image = convert_image(depth_image)
+        predicted_image = convert_image(predicted_image)
         images = np.hstack((depth_image, predicted_image))
 
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+        cv2.waitKey(5)
 
 finally:
 
